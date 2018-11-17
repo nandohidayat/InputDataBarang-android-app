f158bc64dea53716a487565bfe08064bea1fd766
Nando <mnandohidayat@gmail.com>
2018-11-17 19:11:25 +0700

    Initial MainActivity

--- app/src/main/java/com/nandohidayat/app/inputdatabarang/MainActivity.java
+++ app/src/main/java/com/nandohidayat/app/inputdatabarang/MainActivity.java
@@ -1,13 +1,39 @@
 package com.nandohidayat.app.inputdatabarang;
 
+import android.app.ListActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.List;
+
+public class MainActivity extends ListActivity {
+    String dataBarang[] = null;
+    String dB[] = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+//        setContentView(R.layout.activity_main);
+        DatabaseHandler db = new DatabaseHandler(this);
+
+        db.addBarang(new Barang("B0001", "Buku", 5000.0, 6000.0, 100));
+
+        List<Barang> barangList = db.getSemuaBarang();
+        dataBarang = new String[barangList.size()];
+        dB = new String[barangList.size()];
+        int i = 0;
+        for(Barang barang : barangList) {
+            dataBarang[i] = barang.getKodeBarang() +
+                    " | " + barang.getNamaBarang() +
+                    " | " + barang.getHargaBeli() +
+                    " | " + barang.getHargaJual() +
+                    " | " + barang.getStok();
+            dB[i] = barang.getKodeBarang();
+            i++;
+        }
+
+        setListAdapter(new ArrayAdapter<Object>(this, android.R.layout.simple_list_item_1, dataBarang));
+        registerForContextMenu(getListView());
     }
 }
