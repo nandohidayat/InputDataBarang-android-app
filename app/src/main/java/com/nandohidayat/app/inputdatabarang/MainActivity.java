199d499964f03cbfc1c7a0cec8640401e617af87
Nando <mnandohidayat@gmail.com>
2018-11-18 17:45:52 +0700

    delete the unnecessary

--- app/src/main/java/com/nandohidayat/app/inputdatabarang/MainActivity.java
+++ app/src/main/java/com/nandohidayat/app/inputdatabarang/MainActivity.java
@@ -1,22 +1,17 @@
 package com.nandohidayat.app.inputdatabarang;
 
-import android.app.ListActivity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.ContextMenu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Adapter;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-    String dataBarang[] = null;
-    String dB[] = null;
     private ListView listView;
     private CustomAdapter adapter;
     List<Barang> barangList;
@@ -43,8 +38,8 @@ public class MainActivity extends AppCompatActivity {
     public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
         super.onCreateContextMenu(menu, v, menuInfo);
         menu.setHeaderTitle("Action");
-        menu.add(0,0,0,"Tambah");
-        menu.add(0,1,1,"Hapus");
+        menu.add(0, 0, 0, "Tambah");
+        menu.add(0, 1, 1, "Hapus");
     }
 
     @Override
