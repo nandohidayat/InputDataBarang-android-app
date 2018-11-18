bdb0ea155d62bbf2129c78f4966a04a441a3924d
Nando <mnandohidayat@gmail.com>
2018-11-18 13:57:37 +0700

    add Context menu to list

--- app/src/main/java/com/nandohidayat/app/inputdatabarang/MainActivity.java
+++ app/src/main/java/com/nandohidayat/app/inputdatabarang/MainActivity.java
@@ -1,8 +1,14 @@
 package com.nandohidayat.app.inputdatabarang;
 
 import android.app.ListActivity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Adapter;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 
 import java.util.List;
@@ -35,5 +41,39 @@ public class MainActivity extends ListActivity {
 
         setListAdapter(new ArrayAdapter<Object>(this, android.R.layout.simple_list_item_1, dataBarang));
         registerForContextMenu(getListView());
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        menu.setHeaderTitle("Action");
+        menu.add(0,0,0,"Tambah");
+        menu.add(0,1,1,"Hapus");
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        try {
+            switch (item.getItemId()) {
+                case 0: {
+                    Class c = Class.forName("com.nandohidayat.app.inputdatabarang.TambahActivity");
+                    Intent i = new Intent(MainActivity.this, c);
+                    startActivity(i);
+                    break;
+                }
+                case 1: {
+                    DatabaseHandler db = new DatabaseHandler(this);
+                    AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
+                    db.deleteRow(dB[info.position]);
+                    Class c = Class.forName("com.nandohidayat.app.inputdatabarang.MainActivity");
+                    Intent i = new Intent(MainActivity.this, c);
+                    startActivity(i);
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return true;
     }
 }
